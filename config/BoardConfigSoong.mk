aed0297ab84dd2a5779b1042832f0aac5d345ea6
Michael Bestas <mkbestas@lineageos.org>
2025-05-07 20:19:57 +0300

    config: Deprecate BOOTLOADER_MESSAGE_OFFSET
    All users have migrated to soong_config_set.
    
    Change-Id: I0b621c9a92d79a54ab45ca9d7a54e843ce3d39d4

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -89,8 +89,3 @@ endif
 ifneq ($(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB),)
     $(call soong_config_set,power_libperfmgr,mode_extension_lib,$(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB))
 endif
-
-# Recovery
-ifneq ($(BOOTLOADER_MESSAGE_OFFSET),)
-    $(call soong_config_set,lineage_recovery,bootloader_message_offset,$(BOOTLOADER_MESSAGE_OFFSET))
-endif
