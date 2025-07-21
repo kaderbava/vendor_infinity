a92d34ccff50247246e146e6be2f1112b47469a0
Aaron Kling <webgeek1234@gmail.com>
2025-07-21 00:32:21 -0500

    common: Allow vendors to exclude bp paths
    This resolves a couple issues. Allowing bp's that don't resolve to be
    in-tree. And allowing multiple copies of the same target.
    
    Change-Id: I85cfc566e18ea9fe1cb4cd8d70a71ef80517fd41

--- config/common.mk
+++ config/common.mk
@@ -2,6 +2,9 @@
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
 
+# Allow vendor prebuilt repos to exclude themselves from bp scanning
+-include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
+
 PRODUCT_BRAND ?= Project Infinity X
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
