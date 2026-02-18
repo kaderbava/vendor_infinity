LineageOS/android_vendor_lineage

lineage: Move build-manifest.xml build rule into `build/tasks`
* For Android.mk deprecation

Change-Id: I4aba949ae0af7674955d7a2c439d191348b41f3c

build/core/definitions.mk
@@ -7,3 +7,6 @@ endef
 
 # Include board/platform macros
 include vendor/lineage/build/core/utils.mk
+
+# Include default installed modules
+include vendor/lineage/build/core/default_installed_modules.mk

kaderbava/vendor_infinity

build/core/definitions.mk
@@ -7,3 +7,6 @@ endef
 
 # Include board/platform macros
 include vendor/infinity/build/core/utils.mk
+
+# Include default installed modules
+include vendor/infinity/build/core/default_installed_modules.mk